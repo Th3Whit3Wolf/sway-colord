d54365d393aef33dd9af62d56f9807c4a1afa718
David Karrick <the.white.wolf.is.1337@gmail.com>
2021-01-21 14:57:07 +0000

    Support non OSS versions of vscode

--- src/config/vscode.rs
+++ src/config/vscode.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use json5;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, to_string_pretty, Value};
@@ -30,31 +30,52 @@ impl VSCode {
     pub fn is_some(&self) -> bool {
         self.dark_theme.is_some()
             && self.light_theme.is_some()
-            && dirs_next::home_dir()
+            && (dirs_next::home_dir()
                 .expect("Error: unable to find home directory")
                 .join(".config/Code - OSS/User/settings.json")
                 .is_file()
+                || dirs_next::home_dir()
+                    .expect("Error: unable to find home directory")
+                    .join(".config/Code/User/settings.json")
+                    .is_file())
     }
     pub fn change_theme(theme: &str) -> Result<()> {
-        let setting = dirs_next::home_dir()
+        let setting = if dirs_next::home_dir()
             .expect("Error: unable to find home directory")
-            .join(".config/Code - OSS/User/settings.json");
-        let data = fs::read_to_string(setting.clone()).expect("Unable to read file");
+            .join(".config/Code - OSS/User/settings.json")
+            .is_file()
+        {
+            Ok(dirs_next::home_dir()
+                .expect("Error: unable to find home directory")
+                .join(".config/Code - OSS/User/settings.json"))
+        } else if dirs_next::home_dir()
+            .expect("Error: unable to find home directory")
+            .join(".config/Code/User/settings.json")
+            .is_file()
+        {
+            Ok(dirs_next::home_dir()
+                .expect("Error: unable to find home directory")
+                .join(".config/Code/User/settings.json"))
+        } else {
+            Err(anyhow!("Could not find the vscode user directory"))
+        };
+        if let Ok(file) = &setting {
+            let data = fs::read_to_string(file.clone()).expect("Unable to read file");
+            // Parse the string of data into serde_json::Value.
+            let v: Value = json5::from_str(&data).expect("failure to convert");
 
-        // Parse the string of data into serde_json::Value.
-        let v: Value = json5::from_str(&data).expect("failure to convert");
+            let mut v: Value = serde_json::from_str(v.to_string().as_str())?;
 
-        let mut v: Value = serde_json::from_str(v.to_string().as_str())?;
+            match v.get_mut("workbench.colorTheme") {
+                Some(val) => *val = json!(theme),
+                None => println!("Theme not set"),
+            }
 
-        match v.get_mut("workbench.colorTheme") {
-            Some(val) => *val = json!(theme),
-            None => println!("Theme not set"),
-        }
-
-        let mut f = File::create(setting).expect("Unable to create file");
-        f.write_all(to_string_pretty(&v)?.as_bytes())
-            .expect("Unable to write data");
-        f.flush().expect("Error: Flushing  VSCodes settings.json");
+            let mut f = File::create(setting?).expect("Unable to create file");
+            f.write_all(to_string_pretty(&v)?.as_bytes())
+                .expect("Unable to write data");
+            f.flush().expect("Error: Flushing  VSCodes settings.json");
+        };
         Ok(())
     }
 }
