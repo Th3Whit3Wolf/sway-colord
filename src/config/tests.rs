ddbd313d0e672ef3516561f12a898ba1bd17691a
David Karrick <the.white.wolf.is.1337@gmail.com>
2021-01-21 15:04:35 +0000

    Updated test

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -3,39 +3,53 @@ use ron::de::from_str;
 
 const CONFIG: &str = "(
     timechange: Solar(52.4045, 0.5613),
-    alacritty: Alacritty(
-        dark_theme: Some(\"dark\"),
-        light_theme: Some(\"light\")
+    alacritty: Some(
+        Alacritty(
+            dark_theme: Some(\"dark\"),
+            light_theme: Some(\"light\")
+        )
     ),
-    gsettings: GSettings(
-        dark_gtk_theme: Some(\"Space-Dark\"),
-        dark_icon_theme: Some(\"Space-Dark\"),
-        dark_cursor_theme: None,
-        dark_font_name: None,
-        light_gtk_theme: Some(\"Space-Light\"),
-        light_icon_theme: Some(\"Space-Light\"),
-        light_cursor_theme: None,
-        light_font_name: None,
+    bat: Some(
+        Bat(
+            dark_theme: Some(\"dark\"),
+            light_theme: Some(\"light\")
+        )
     ),
-    lighting: Lighting(
-        monitor: Some(
-            Monitor(
-                device: \"amdgpu_bl0\",
-                light_perc: 50,
-                dark_perc: 20
-            )
-        ),
-        keyboard: Some(
-            Keyboard(
-                device: \"asus::kbd_backlight\",
-                light_perc: 0,
-                dark_perc: 34
+    gsettings: Some(
+        GSettings(
+            dark_gtk_theme: Some(\"Dark\"),
+            dark_icon_theme: Some(\"Dark\"),
+            dark_cursor_theme: None,
+            dark_font_name: None,
+            light_gtk_theme: Some(\"Light\"),
+            light_icon_theme: Some(\"Light\"),
+            light_cursor_theme: None,
+            light_font_name: None,
+        )
+    ),
+    lighting: Some(
+        Lighting(
+            monitor: Some(
+                Monitor(
+                    device: \"amdgpu_bl0\",
+                    light_perc: 50,
+                    dark_perc: 20
+                )
+            ),
+            keyboard: Some(
+                Keyboard(
+                    device: \"kbd_backlight\",
+                    light_perc: 0,
+                    dark_perc: 34
+                )
             )
         )
     ),
-    vscode: VSCode(
-        dark_theme: Some(\"Spacemacs - dark\"),
-        light_theme: Some(\"Spacemacs - light\")
+    vscode: Some(
+        VSCode(
+            dark_theme: Some(\"Dark\"),
+            light_theme: Some(\"Light\")
+        )
     )
 )";
 
@@ -46,8 +60,14 @@ fn test_read_config() {
     if !conf.alacritty.is_some() {
         panic!("Alacritty not detected")
     }
+    if !conf.bat.is_some() {
+        panic!("VSCode not detected")
+    }
     if !conf.gsettings.is_some() {
         panic!("GSettings not detected")
+    }
+    if !conf.lighting.is_some() {
+        panic!("VSCode not detected")
     }
     if !conf.vscode.is_some() {
         panic!("VSCode not detected")
