5085daf6f3940eabba6c068463d47504ede17e65
David Karrick <the.white.wolf.is.1337@gmail.com>
2021-01-23 22:01:23 +0000

    Changed load behavior

--- src/main.rs
+++ src/main.rs
@@ -71,8 +71,7 @@ async fn auto_change_solar(conf: Config, lattitude: f64, longitude: f64) -> Resu
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    //Config::lint_config();
-    let conf = Config::load();
+    let conf = Config::load()?;
     match conf.timechange.clone() {
         config::TimeChange::Rigid(morning, night) => {
             let dawn = NaiveTime::parse_from_str(&morning, "%H:%M:%S")
