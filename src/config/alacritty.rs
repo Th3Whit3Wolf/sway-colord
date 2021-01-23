0cb4dce1e6cf3cacef96aa0ed586ac14fef0da9c
David Karrick <the.white.wolf.is.1337@gmail.com>
2021-01-23 21:57:48 +0000

    Change theme is no longer a method

--- src/config/alacritty.rs
+++ src/config/alacritty.rs
@@ -13,31 +13,32 @@ const ALACRITTY_SETTINGS_STARTS_WITH: &str = "colors";
 impl Alacritty {
     pub fn dark_mode(&self) -> Result<()> {
         if let Some(theme) = &self.dark_theme {
-            Alacritty::change_theme(theme.as_str())?;
+            change_theme(theme.as_str())?;
         }
         Ok(())
     }
     pub fn light_mode(&self) -> Result<()> {
         if let Some(theme) = &self.light_theme {
-            Alacritty::change_theme(theme.as_str())?;
+            change_theme(theme.as_str())?;
         }
-        Ok(())
-    }
-    pub fn change_theme(theme: &str) -> Result<()> {
-        let setting = dirs_next::home_dir().expect("Error: unable to find home directory").join(".config/alacritty/alacritty.yml");
-        let theme_insert = format!(
-            "{}: *{}",
-            ALACRITTY_SETTINGS_STARTS_WITH,
-            theme
-        );
-        theme_file(
-            setting,
-            ALACRITTY_SETTINGS_STARTS_WITH,
-            theme_insert,
-        )?;
         Ok(())
     }
     pub fn is_some(&self) -> bool {
         self.dark_theme.is_some() && self.light_theme.is_some() && dirs_next::home_dir().expect("Error: unable to find home directory").join(".config/alacritty/alacritty.yml").is_file()
     }
+}
+
+fn change_theme(theme: &str) -> Result<()> {
+    let setting = dirs_next::home_dir().expect("Error: unable to find home directory").join(".config/alacritty/alacritty.yml");
+    let theme_insert = format!(
+        "{}: *{}",
+        ALACRITTY_SETTINGS_STARTS_WITH,
+        theme
+    );
+    theme_file(
+        setting,
+        ALACRITTY_SETTINGS_STARTS_WITH,
+        theme_insert,
+    )?;
+    Ok(())
 }
